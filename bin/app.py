fcb6795bfb5e53efb5ad178faa0c6dc00a11983c
xnanodax <cynthia.ma.emily@gmail.com>
2016-09-19 20:57:22 -0700

    Create app.py

---
+++ bin/app.py
@@ -0,0 +1,21 @@
+import web
+
+urls = (
+	'/', 'index'
+)
+
+app = web.application(urls, globals())
+app = app.wsgifunc()
+
+render = web.template.render('/home/yourUSERNAME/yourWEBSITE/templates/')
+	#type in your python anywhere USERNAME
+	#type in your python anywhere WEBAPP Folder's name
+	#Ex. /home/missmascience/websitev1/templates/
+
+class index(object):
+	def GET(self):
+		greeting = "Hello World"
+		return render.index(greeting=greeting)
+
+if __name__ == "__main__":
+	app.run()
